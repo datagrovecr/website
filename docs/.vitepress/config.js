765247e040a4340229e128daa0a593a727be06e1
Meagan <meagan.h.smith@gmail.com>
2022-08-22 16:16:53 -0500

    Simplify jump links for mobile

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -14,10 +14,10 @@ export default defineConfig({
             { 
                 text: 'Join Us', 
                 items: [
-                    { text: "Working for Datagrove Costa Rica", link: '/jobs/'},
+                    // { text: "Working for Datagrove Costa Rica", link: '/jobs/'},
                     { text: "Full-Time Positions", link: '/jobs/#full-time' },
                     { text: "Internships", link: '/jobs/#internships' },
-                    { text: "Application", link: '/jobs/#application'}
+                    // { text: "Application", link: '/jobs/#application'}
                 ],
             },
         ],
